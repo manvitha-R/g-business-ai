33d0b8876b6ac0418b45dd645a8f8da43b8b1c0d
Manvitha R <107823807+manvitha-R@users.noreply.github.com>
2025-02-06 15:19:58 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 // import Image from "next/image";
 "use client"
 import Sidebar from "./components/sidebar";
-import Popup from "./components/Popup";
-import Container from "./Container/container";
+// import Popup from "./components/Popup";
+// import Container from "./Container/container";
 import Dashboard from "./Dashboard/dashboard";
-import Managetable from "./Managetable/managetable";
+// import Managetable from "./Managetable/managetable";
 
 
 export default function Home() {
